90dde54a8b99aed3eb089ec75180657b08cc9f3d
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2022-06-05 11:51:07 +0530

    barebones working model

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import React, { useState } from 'react';
+//mock data
 import data from "./data.json";
 //components
 import Header from "./Header";
-import ToDoList from './ToDoList';
-import './App.css';
- 
+import ToDoList from "./ToDoList";
+import ToDoForm from './ToDoForm';
+
 function App() {
+  
   const [ toDoList, setToDoList ] = useState(data);
- 
- 
- return (
-   <div className="App">
-     <Header />
-     <ToDoList toDoList={toDoList}/>
-   </div>
- );
-}
 
-const handleToggle = (id) => {
-  let mapped  = toDoList.map(task => {
-    return task.id == id ? { ...task, complete: !task.complete } : { ...task};
-  });
-  setToDoList(mapped);
-}
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+    });
+    setToDoList(mapped);
+  }
 
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
 
-const handleFilter = () => {
-  let filtered = toDoList.filter(task => {
-    return !task.complete;
-  });
-  setToDoList(filtered);
+  const addTask = (userInput ) => {
+    let copy = [...toDoList];
+    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+    setToDoList(copy);
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+      <ToDoForm addTask={addTask}/>
+    </div>
+  );
 }
 
 export default App;
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React from 'react';
 import ToDo from './ToDo';
-
 
 const ToDoList = ({toDoList, handleToggle, handleFilter}) => {
     return (
         <div>
             {toDoList.map(todo => {
                 return (
-                    <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter} />                
+                    <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter}/>
                 )
             })}
             <button style={{margin: '20px'}} onClick={handleFilter}>Clear Completed</button>
